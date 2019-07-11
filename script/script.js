6b3aee3c9af6aa0ef9f82b17ff59b7b01a7870e8
YaroshYevhen <eeugenie1992@gmail.com>
2019-07-11 16:49:04 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -135,7 +135,7 @@ APP.$document.ready(function() {
 	});
 
 	APP.scrollUp.on('click', function(){
-		$('html').scrollTop(0);
+		$('html').animate({ scrollTop: 0 }, 500);
 	});
 
 	APP.slider.each(function(key, item) {
